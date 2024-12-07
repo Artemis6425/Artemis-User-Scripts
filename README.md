19159000eab6bd73df819bbd5397574acff0f391
unknown <aurorarose6425@gmail.com>
2024-12-07 14:21:18 -0500

    Amazon URL Cleaner v1.0

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Various user scrips I've modified to work the way I want to
 Credit will be given to where I get them, and I will also list what changes I made
 
 ## Amazon URL Cleaner
-### Install link: here
+### Install link: [here](https://github.com/Artemis6425/Artemis-User-Scripts/raw/refs/heads/main/scripts/Amazon%20URL%20Cleaner.user.js)
 ### Description
 This fixes the Amazon URL to remove all that tracking garbage so you can share a short link.
 ### Changes
--- scripts/Amazon URL Cleaner.user.js
+++ scripts/Amazon URL Cleaner.user.js
@@ -2,14 +2,15 @@
 // @id             Amazon URL Cleaner
 // @name           Amazon URL Cleaner
 // @namespace      https://github.com/Artemis6425/Artemis-User-Scripts
-// @description    replaceState for Amazon
+// @author         azu 2 & Artemis6425
+// @description    Removes URL tracking for Amazon URLs
 // @include        https://www.amazon.*/dp/*
 // @include        https://www.amazon.*/*gp/product/*
 // @include        https://www.amazon.*/exec/obidos/ASIN/*
 // @include        https://www.amazon.*/o/ASIN/*
 // @version        1.0
-// @downloadURL    
-// @updateURL      
+// @downloadURL    https://github.com/Artemis6425/Artemis-User-Scripts/raw/refs/heads/main/scripts/Amazon%20URL%20Cleaner.user.js
+// @updateURL      https://github.com/Artemis6425/Artemis-User-Scripts/raw/refs/heads/main/scripts/Amazon%20URL%20Cleaner.user.js
 // @run-at         document-end
 // ==/UserScript==
 (function(doc) {
