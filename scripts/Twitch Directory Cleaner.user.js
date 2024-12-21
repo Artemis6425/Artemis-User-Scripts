003f94655629c9b965f98b9f6ec5c045841d1db5
unknown <aurorarose6425@gmail.com>
2024-12-21 13:05:09 -0500

    +1 user

--- scripts/Twitch Directory Cleaner.user.js
+++ scripts/Twitch Directory Cleaner.user.js
@@ -2,7 +2,7 @@
 // @id           Twitch Directory Cleaner
 // @name         Twitch Directory Cleaner
 // @namespace    https://github.com/Artemis6425/Artemis-User-Scripts
-// @version      1.10
+// @version      1.10.1
 // @updateURL    https://github.com/Artemis6425/Artemis-User-Scripts/raw/refs/heads/main/scripts/Twitch%20Directory%20Cleaner.user.js
 // @downloadURL  https://github.com/Artemis6425/Artemis-User-Scripts/raw/refs/heads/main/scripts/Twitch%20Directory%20Cleaner.user.js
 // @description  Removes Channels from any directory if they are found on a list.
@@ -48,7 +48,8 @@
 		"247chrism",
 		"shyguyjj",
 		"zockerstuebchen_tv",
-		"checkpoint_radio"
+		"checkpoint_radio",
+		"pixelstation64"
     ]
 
 
